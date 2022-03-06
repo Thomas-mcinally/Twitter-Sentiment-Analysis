d319f8b6549d749916166ab6bb62a3e5a8c0d9d4
Thomas-mcinally <95553476+Thomas-mcinally@users.noreply.github.com>
2022-03-06 10:55:51 +0000

    init

--- powerlifting/app.py
+++ powerlifting/app.py
@@ -1,9 +1,14 @@
+'''
+Tutorial series to use: https://www.youtube.com/watch?v=mqhxxeeTbu0&list=PLzMcBGfZo4-n4vJJybUVV3Un_NFS5EOgX
+'''
+
+
 from flask import Flask
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return "<p>Hello, World!</p>"
+    return "<p>Hello world!</p>"
 
 
