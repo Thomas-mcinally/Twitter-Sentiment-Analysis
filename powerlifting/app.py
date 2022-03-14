954d7384e000b5ce98ab26afc51e0c0002de7bec
Thomas-mcinally <95553476+Thomas-mcinally@users.noreply.github.com>
2022-03-14 21:51:03 +0000

    update2

--- powerlifting/app.py
+++ powerlifting/app.py
@@ -11,6 +11,10 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route("/deadlifts")
+def deadlifts():
+    return render_template('deadlifts.html')
+
 
 if __name__=='__main__':
     app.run(debug=True)
