8c3704e4cb7c385db150728bb55363fb92bf28c7
Thomas-mcinally <95553476+Thomas-mcinally@users.noreply.github.com>
2022-03-22 18:12:25 +0000

    day1

---
+++ sentiment_analysis/Notes.py
@@ -0,0 +1,40 @@
+#Example sentences and their sentiment
+'''
+SIMPLE CASES
+Coronet has the best lines of all day cruisers.
+Bertram has a deep V hull and runs easily through seas.
+Pastel-colored 1980s day cruisers from Florida are ugly.
+I dislike old cabin cruisers.
+
+ADVANCED CASES
+I do not dislike cabin cruisers. (Negation handling)
+Disliking watercraft is not really my thing. (Negation, inverted word order)
+Sometimes I really hate RIBs. (Adverbial modifies the sentiment)
+I'd really truly love going out in this weather! (Possibly sarcastic)
+Chris Craft is better looking than Limestone. (Two brand names, identifying the target of attitude is difficult).
+Chris Craft is better looking than Limestone, but Limestone projects seaworthiness and reliability. (Two attitudes, two brand names).
+The movie is surprising with plenty of unsettling plot twists. (Negative term used in a positive sense in certain domains).
+You should see their decadent dessert menu. (Attitudinal term has shifted polarity recently in certain domains)
+I love my mobile but would not recommend it to any of my colleagues. (Qualified positive sentiment, difficult to categorise)
+Next week's gig will be right koide9! ("Quoi de neuf?", French for "what's new?". Newly minted terms can be highly attitudinal but volatile in polarity and often out of known vocabulary.)
+'''
+
+
+#COMMON SENTIMENT ANALYSIS LIBRARIES
+'''
+Rule-based models (No ML)
+- vader - Optimised for social-media
+- textblob
+
+ML-Based models
+flair - Pretrained model is trained on IMDB dataset
+transformers (BERT - Hugging face) - Multi-lingual sentiment analysis tool optimised for product reviews
+
+Check out this video for how they compare: https://www.youtube.com/watch?v=q0V_z_I9bWU
+Good article: https://neptune.ai/blog/sentiment-analysis-python-textblob-vs-vader-vs-flair
+
+Key points:
+- Transformers and Flair have significantly longer processing time - dont use if time sensitive
+- Different models are trained on different data - the best model to use depends on the text you are studying
+
+'''
--- sentiment_analysis/TODO.py
+++ sentiment_analysis/TODO.py
@@ -1,24 +1,6 @@
-#Test out Sentiment analysis API with some examples to quality control
-'''
-SIMPLE CASES
-Coronet has the best lines of all day cruisers.
-Bertram has a deep V hull and runs easily through seas.
-Pastel-colored 1980s day cruisers from Florida are ugly.
-I dislike old cabin cruisers.
+#Do sentiment analysis on current dataset using VADER
+#Get larger dataset and repeat whole process
 
-ADVANCED CASES
-I do not dislike cabin cruisers. (Negation handling)
-Disliking watercraft is not really my thing. (Negation, inverted word order)
-Sometimes I really hate RIBs. (Adverbial modifies the sentiment)
-I'd really truly love going out in this weather! (Possibly sarcastic)
-Chris Craft is better looking than Limestone. (Two brand names, identifying the target of attitude is difficult).
-Chris Craft is better looking than Limestone, but Limestone projects seaworthiness and reliability. (Two attitudes, two brand names).
-The movie is surprising with plenty of unsettling plot twists. (Negative term used in a positive sense in certain domains).
-You should see their decadent dessert menu. (Attitudinal term has shifted polarity recently in certain domains)
-I love my mobile but would not recommend it to any of my colleagues. (Qualified positive sentiment, difficult to categorise)
-Next week's gig will be right koide9! ("Quoi de neuf?", French for "what's new?". Newly minted terms can be highly attitudinal but volatile in polarity and often out of known vocabulary.)
+#Create sentiment versus time plot for period around NYT acquisition
 
 
-
-
-'''
