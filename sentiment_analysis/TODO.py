2c2aa4d2a02c166132ee1626a9feb20fc7a74ef6
Thomas-mcinally <95553476+Thomas-mcinally@users.noreply.github.com>
2022-03-23 21:50:10 +0000

    update

--- sentiment_analysis/TODO.py
+++ sentiment_analysis/TODO.py
@@ -1,5 +1,7 @@
-#Do sentiment analysis on current dataset using VADER
-#Get larger dataset and repeat whole process
+#Refine API search query and get mediu-size dataset
+    #Geolocation - UK
+
+#Use vader for sentiment analysis of larger dataset
 
 #Create sentiment versus time plot for period around NYT acquisition
 
